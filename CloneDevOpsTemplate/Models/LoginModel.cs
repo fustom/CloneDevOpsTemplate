54cf7e04c122ea3d78cd1836196738c3b502cf64
fustom <fustom@gmail.com>
2025-03-13 10:54:10 +0100

    Add login fields validation

--- CloneDevOpsTemplate/Models/LoginModel.cs
+++ CloneDevOpsTemplate/Models/LoginModel.cs
@@ -4,10 +4,12 @@ namespace CloneDevOpsTemplate.Models;
 
 public class LoginModel
 {
-    [Required]
+    [Required(ErrorMessage = "Organization name is required.")]
+    [Display(Name = "Organization name")]
     public string OrganizationName { get; set; } = string.Empty;
 
-    [Required]
+    [Required(ErrorMessage = "Access token is required.")]
     [DataType(DataType.Password)]
+    [Display(Name = "Access token")]
     public string AccessToken { get; set; } = string.Empty;
 }
