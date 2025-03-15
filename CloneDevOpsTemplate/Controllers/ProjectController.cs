f773fc4b27d770d6a880dd16a4e9c5b1d4c6d405
Tamás Füstös <fustom@gmail.com>
2025-03-15 21:39:09 +0100

    Iteration path must be stored to move the right place (#18)

--- CloneDevOpsTemplate/Controllers/ProjectController.cs
+++ CloneDevOpsTemplate/Controllers/ProjectController.cs
@@ -56,7 +56,7 @@ public class ProjectController(IProjectService projectService, IIterationService
         }
 
         Iteration templateIterations = await _iterationService.GetIterationsAsync(templateProjectId) ?? new();
-        Iteration iterations = await _iterationService.CreateIterationAsync(project.Id, templateIterations);
+        Iteration iterations = await _iterationService.CreateIterationAsync(project.Id, project.Name, templateIterations);
         await _iterationService.MoveIteration(project.Id, iterations);
 
         return RedirectToAction("Project", new { projectId = project.Id });
