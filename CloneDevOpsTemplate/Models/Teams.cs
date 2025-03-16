71b4ecd4a9ceb805117a84497bc1d2a5747566b3
Tamás Füstös <fustom@gmail.com>
2025-03-16 21:21:37 +0100

    Use includeCapabilities=true for GetProjectAsync (#20)
    * Use includeCapabilities=true for GetProjectAsync
    
    * Fix Project views

--- CloneDevOpsTemplate/Models/Teams.cs
+++ CloneDevOpsTemplate/Models/Teams.cs
@@ -6,11 +6,15 @@ public class Teams
     public Team[] Value { get; set; } = [];
 }
 
-public class Team
+public class BaseTeam
 {
     public string Id { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public string Url { get; set; } = string.Empty;
+}
+
+public class Team : BaseTeam
+{
     public string Description { get; set; } = string.Empty;
     public string IdentityUrl { get; set; } = string.Empty;
     public string ProjectName { get; set; } = string.Empty;
