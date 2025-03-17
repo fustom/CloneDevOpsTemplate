77935bf51c29eba7691c9ae20725a948313a6c3a
fustom <fustom@gmail.com>
2025-03-17 15:24:10 +0100

    Add team settings

---
+++ CloneDevOpsTemplate/Services/ITeamSettingsService.cs
@@ -0,0 +1,9 @@
+using CloneDevOpsTemplate.Models;
+
+namespace CloneDevOpsTemplate.Services;
+
+public interface ITeamSettingsService
+{
+    Task<TeamSettings?> GetTeamSettings(Guid projectId, Guid teamId);
+    Task UpdateTeamSettings(Guid projectId, Guid teamId, TeamSettings teamSettings);
+}
