ad957cb1c5d78634372197f6abcae45e4736e18d
József Szalontai <57400093+j0zzz@users.noreply.github.com>
2025-03-19 20:52:39 +0100

    View boards (#32)
    * Minimal view of board columns and rows
    
    * Boards view styling

---
+++ CloneDevOpsTemplate/Controllers/BoardsController.cs
@@ -0,0 +1,29 @@
+﻿using CloneDevOpsTemplate.Models;
+using CloneDevOpsTemplate.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloneDevOpsTemplate.Controllers
+{
+    public class BoardsController(IBoardService boardService) : Controller
+    {
+        private readonly IBoardService _boardService = boardService;
+
+        async public Task<IActionResult> Boards(Guid projectId, Guid teamId)
+        {
+            Boards boardValues = await _boardService.GetBoardsAsync(projectId, teamId) ?? new Boards();
+            List<Board> boards = new List<Board>();
+            foreach (var board in boardValues.Value)
+            {
+                var currentBoard = await _boardService.GetBoardAsync(projectId, teamId, board.Id);
+                if (currentBoard != null)
+                {
+                    // Query rows separately, since row colors are ALWAYS null from the GetBoard call
+                    var rows = await _boardService.GetBoardRowsAsync(projectId, teamId, currentBoard.Id);
+                    currentBoard.Rows = rows.Value;
+                    boards.Add(currentBoard);
+                }
+            }
+            return View(boards.ToArray());
+        }
+    }
+}
