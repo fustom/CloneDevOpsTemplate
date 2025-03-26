db00323cf142fe6611a039c9cb3d239a2e524cae
fustom <fustom@gmail.com>
2025-03-26 10:30:30 +0100

    Add task reponse type for services where needed

--- CloneDevOpsTemplate/IServices/IBoardService.cs
+++ CloneDevOpsTemplate/IServices/IBoardService.cs
@@ -8,11 +8,11 @@ public interface IBoardService
     Task<Board?> GetBoardAsync(Guid projectId, Guid teamId, string boardId);
 
     Task<BoardColumns?> GetBoardColumnsAsync(Guid projectId, Guid teamId, string boardId);
-    Task UpdateBoardColumnsAsync(Guid projectId, Guid teamId, string boardId, BoardColumns boardColumns);
+    Task<HttpResponseMessage> UpdateBoardColumnsAsync(Guid projectId, Guid teamId, string boardId, BoardColumns boardColumns);
     Task MoveBoardColumnsAsync(Guid projectId, Guid projectTeamId, Guid templateProjectId, Guid templateTeamId, Boards projectBoards);
 
     Task<BoardRows?> GetBoardRowsAsync(Guid projectId, Guid teamId, string boardId);
-    Task UpdateBoardRowsAsync(Guid projectId, Guid teamId, string boardId, BoardRows boardRows);
+    Task<HttpResponseMessage> UpdateBoardRowsAsync(Guid projectId, Guid teamId, string boardId, BoardRows boardRows);
     Task MoveBoardRowsAsync(Guid projectId, Guid projectTeamId, Guid templateProjectId, Guid templateTeamId, Boards projectBoards);
 
     Task<CardSettings?> GetCardSettingsAsync(Guid projectId, Guid teamId, string boardId);
