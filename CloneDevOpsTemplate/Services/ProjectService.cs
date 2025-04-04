379ed09645b97d349f07e1d6ecb781dc27acd134
Tamás Füstös <fustom@gmail.com>
2025-04-04 16:28:02 +0200

    Removed unused Process(es) model and controller action (#107)

--- CloneDevOpsTemplate/Services/ProjectService.cs
+++ CloneDevOpsTemplate/Services/ProjectService.cs
@@ -30,16 +30,6 @@ public class ProjectService(IHttpClientFactory httpClientFactory) : IProjectServ
         return _client.GetFromJsonAsync<ProjectProperties>($"_apis/projects/{projectId}/properties");
     }
 
-    public Task<Processes?> GetProcessesAsync()
-    {
-        return _client.GetFromJsonAsync<Processes>($"_apis/work/processes");
-    }
-
-    public Task<Process?> GetProcessAsync(string processTemplateType)
-    {
-        return _client.GetFromJsonAsync<Process>($"_apis/work/processes/{processTemplateType}");
-    }
-
     public async Task<CreateProjectResponse?> CreateProjectAsync(string name, string description, string processTemplateType, string sourceControlType, Visibility visibility)
     {
         CreateProject createProject = new()
