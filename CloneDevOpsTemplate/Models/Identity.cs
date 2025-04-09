9adad4fb8595e69221ec3b31e6413753962bcbe1
j0zzz <joequake@gmail.com>
2025-04-09 08:00:09 +0000

    Removed TeamMember class and replaced it with IdentityRef

--- CloneDevOpsTemplate/Models/Identity.cs
+++ CloneDevOpsTemplate/Models/Identity.cs
@@ -6,6 +6,8 @@ public class IdentityRef
     public string Url { get; set; } = string.Empty;
     public string Descriptor { get; set; } = string.Empty;
     public string DirectoryAlias { get; set; } = string.Empty;
-    public string Id { get; set; } = string.Empty;
+    public Guid Id { get; set; }
     public bool IsDeletedInOrigin { get; set; }
+    public string UniqueName { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
 }
